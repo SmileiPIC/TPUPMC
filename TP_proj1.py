44cbffe1ccba02bb8e06ed6b3d3d7c497c226c69
tommaso vinci <tommaso.vinci@polytechnique.edu>
2016-11-04 17:27:28 +0100

    cleanup

--- TP_proj1.py
+++ TP_proj1.py
@@ -1,15 +1,15 @@
-my_every=10
-npart=10
+output_every=10
+n_part=10
 
 Main(
     geometry = "1d3v",
     interpolation_order = 2,
     sim_time = 30.0,
-    cell_length = [0.01],
     timestep = 0.01,
     sim_length  = [1.0],
+    cell_length = [0.01],
     number_of_patches = [ 2 ],
-    print_every = my_every,
+    print_every = output_every,
     bc_em_type_x = ['silver-muller'],
 )
 
@@ -18,7 +18,7 @@ Species(
     nb_density = 1.0,
     initPosition_type = 'regular',
     initMomentum_type = 'cold',
-    n_part_per_cell = npart,
+    n_part_per_cell = n_part,
     mass = 1836.0,
     charge = 1.0,
     time_frozen = 10000.0,
@@ -30,7 +30,7 @@ Species(
     species_type = "eon",
     initPosition_type = "regular",
     initMomentum_type = "cold",
-    n_part_per_cell = npart,
+    n_part_per_cell = n_part,
     mass = 1.0,
     charge = -1.0,
     nb_density = cosine(1,xamplitude=0.1,xnumber=1),
@@ -40,13 +40,13 @@ Species(
 
 DiagScalar (
     precision = 3,
-    every = my_every,
+    every = output_every,
     vars = ['Utot', 'Ukin', 'Uelm', 'Ukin_eon', 'Ntot_eon', 'Uelm_Ex']
 ) 
  
 DiagParticles(
     output = "density",
-    every = my_every,
+    every = output_every,
     species = ["eon"],
     axes = [
         ["x", 0., 1., 100],
@@ -55,7 +55,7 @@ DiagParticles(
 )
 
 DiagFields(
-    every = my_every,
+    every = output_every,
     fields = ['Jx', 'Rho_eon']
 )
 
--- TP_proj2.py
+++ TP_proj2.py
@@ -1,16 +1,17 @@
-my_every=20
-npart=10
+output_every=20
+n_part=10
 velocity=0.02
 amplitude=0.02
+
 Main(
     geometry = "1d3v",
     interpolation_order = 2,
     sim_time = 50.0,
-    cell_length = [0.02],
     timestep = 0.02,
     sim_length  = [1.0],
+    cell_length = [0.02],
     number_of_patches = [ 2 ],
-    print_every = my_every,
+    print_every = output_every,
     bc_em_type_x = ['periodic'],
     poisson_iter_max=0
 )
@@ -19,7 +20,7 @@ Species(
     species_type = 'ion',
     initPosition_type = 'regular',
     initMomentum_type = 'cold',
-    n_part_per_cell = npart,
+    n_part_per_cell = n_part,
     mass = 1836.0,
     charge = 1.0,
     nb_density = 1.0,
@@ -32,7 +33,7 @@ Species(
     species_type = "eon1",
     initPosition_type = "regular",
     initMomentum_type = "cold",
-    n_part_per_cell = npart/2,
+    n_part_per_cell = n_part/2,
     mass = 1.0,
     charge = -1.0,
     nb_density = cosine(0.5,xamplitude=amplitude,xnumber=1),
@@ -45,7 +46,7 @@ Species(
     species_type = "eon2",
     initPosition_type = "regular",
     initMomentum_type = "cold",
-    n_part_per_cell = npart/2,
+    n_part_per_cell = n_part/2,
     mass = 1.0,
     charge = -1.0,
     nb_density = cosine(0.5,xamplitude=amplitude,xnumber=1),
@@ -56,13 +57,13 @@ Species(
 
 DiagScalar (
     precision = 3,
-    every=my_every,
+    every=output_every,
     vars = ['Utot', 'Ukin', 'Uelm', 'Ukin_eon1', 'Ukin_eon2', 'Uelm_Ex']
 )
  
 DiagParticles(
     output = "density",
-    every = my_every,
+    every = output_every,
     species = ['eon1', 'eon2'],
     axes = [
         ["x", 0., 1., 100],
@@ -71,7 +72,7 @@ DiagParticles(
 )
 
 DiagFields(
-    every = my_every,
+    every = output_every,
     fields = ['Jx', 'Rho_eon1', 'Rho_eon2']
 )
 
